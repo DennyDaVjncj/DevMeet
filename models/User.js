759efcf6241d52d98cf008504f78412c67ad9f0f
Denny da Vjncj <daVjncjCode@gmail.com>
2021-03-04 23:19:23 -0500

    version control

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+     required:"Username is Required"
+    },
+    language: {
+        type: String,
+        trim: true,
+    },
+    password: {
+        type: String,
+        trim: true,
+        require: "Password is Required",
+        validate: [({ length }) => length >= 6, "Password should be longer."]
+    },
+    email : {
+        type: String, 
+       unique :true, 
+       match: [/.+@.+\..+/, "Please enter a valid e-mail address"]
+    },
+    userCreated: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.models.User || mongoose.model('User', userSchema)
+
+
+module.exports = User;
+//testing
